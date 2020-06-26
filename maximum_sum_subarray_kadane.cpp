bec6d41e212b70885e9cbe2dd950f4fdd5d3c8b9
Aviral <61791180+aviral10@users.noreply.github.com>
2020-06-26 19:39:30 +0530

    Create maximum_sum_subarray_kadane.cpp

---
+++ maximum_sum_subarray_kadane.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+//Kadane's algorithm for finding the subarray with maximum sum
+int max_sum_array(vector<int> &a)
+{
+    int sum = 0, best = 0;
+    for(int i=0;i<a.size();i++)
+    {
+        sum = max(a[i], sum+a[i]);
+        best = max(best, sum);
+    }
+    return best;
+}
+
+
+int main()
+{
+    vector<int> arr = {-1,2,4,-3,5,2,-5,2};
+    int val;
+    val = max_sum_array(arr);
+    cout << val << '\n';
+    
+    return 0;
+}
