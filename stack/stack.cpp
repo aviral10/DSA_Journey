3d2119160abfb05432a2bcbea466dee9b23761db
Aviral <61791180+aviral10@users.noreply.github.com>
2020-07-01 20:14:52 +0530

    incomplete

---
+++ stack/stack.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+typedef vector<int>		vi;
+
+//Basic Stack
+
+class Stack{
+    public:
+        vector<int> stack;
+        int top;
+        Stack()
+        {
+            top = -1;
+        }
+
+        void push(int val)
+        {
+            stack.push_back(val);
+            top++;
+        }
+
+        int isEmpty()
+        {
+            return top == -1;
+        }
+
+        int pop()
+        {
+            if(isEmpty())
+                return -1;
+            int temp;
+            temp = stack[top--];
+            stack.pop_back();
+            return temp;
+        }
+
+};
+
+
+
+int main()
+{
+    
+    return 0;
+}
