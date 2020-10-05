43b58737747778017684a1e2a9205fcb453bc1ed
Panda <aviral19rana@gmail.com>
2020-10-05 15:17:16 +0530

    Added to BST

--- BST/BinarySearchTree.cpp
+++ BST/BinarySearchTree.cpp
@@ -34,6 +34,44 @@ private:
         return root;
     }
 
+    int inorder_successor(Node* root){
+        Node* t = root;
+        while(t->lchild){
+            t = t->lchild;
+        }
+        return t->data;
+    }
+
+    Node* Delete(Node* root, int val){
+        if(root == NULL) return root;
+        if     (val < root->data) root->lchild = Delete(root->lchild, val);
+        else if(val > root->data) root->rchild = Delete(root->rchild, val);
+        else{
+            //Case 1: No child
+            if(root->lchild == NULL && root->rchild == NULL){
+                delete root;
+                root = NULL;
+                return root;
+            }
+            //Case 2: Exactly 1 child
+            if(root->lchild == NULL){
+                Node* t = root;
+                delete t;
+                root = root->rchild;
+                return root;
+            }else if(root->rchild == NULL){
+                Node* t = root;
+                delete t;
+                root = root->lchild;
+                return root;
+            }
+            //Case 3: Two child
+            root->data = inorder_successor(root->rchild);
+            Delete(root->rchild, root->data);
+            return root;
+        }
+    }
+
     void inorder(Node* r){
         if(r==NULL) return;
         inorder(r->lchild);
@@ -52,6 +90,10 @@ public:
         root = insert(this->root, val);
     }
 
+    void Delete(int val){
+        root = Delete(root, val);
+    }
+
     void inorder(){
         inorder(this->root);
         cout << '\n';
@@ -64,11 +106,15 @@ public:
 
 int main(){
     BinarySearchTree b;
-    b.insert(10);
-    b.insert(-10);
     b.insert(20);
-    b.insert(-20);
-    b.insert(-15);
+    b.insert(10);
+    b.insert(30);
+    b.insert(5);
+    b.insert(15);
+    b.insert(25);
+    b.insert(35);
+    b.inorder();
+    b.Delete(20);
     b.inorder();
     return 0;
 }
